c8c991015fd4fea758783cdbe7aed6ec59ee15bd
Hcy04 <c2769532417@163.com>
2024-09-28 19:18:28 +0800

    Crystal explode and move

--- Assets/Scripts/Entity/Projectile/Crystal/Crystal.cs
+++ Assets/Scripts/Entity/Projectile/Crystal/Crystal.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Crystal : Projectile
 {
+    private Collider2D cd;
+
     public float crystalTimer;
-    [HideInInspector] public bool triggerCalled;
+    public bool triggerCalled;
+    public float attackCheckRadius;
+
+    private bool canMoving;
+    private float moveSpeed;
+    private Transform clocestTarget;
 
     protected override void Awake()
     {
@@ -15,20 +22,70 @@ public class Crystal : Projectile
     protected override void Start()
     {
         base.Start();
+        
+        cd = GetComponent<Collider2D>();
+        cd.enabled = false;
     }
 
     protected override void Update()
     {
         base.Update();
 
-        if (crystalTimer >= 0) crystalTimer -= Time.deltaTime;
-        else anim.SetTrigger("Destroy");
+        if (crystalTimer >= 0) 
+        {
+            crystalTimer -= Time.deltaTime;
+
+            if (canMoving)
+            {
+                FindTarget();
+                if (clocestTarget != null)
+                {
+                    rb.velocity = (clocestTarget.position - transform.position).normalized * moveSpeed;
+                    if (Vector2.Distance(transform.position, clocestTarget.position) < 1) DestroyCrystal();
+                }
+            }
+        }
+        else DestroyCrystal();
 
         if (triggerCalled) Destroy(this.gameObject);
     }
 
-    public void SetUpCrystal(float _crystalDuration)
+    public void SetUpCrystal(float _crystalDuration, float _attackCheckRadius, bool _canMoving, float _moveSpeed)
     {
         crystalTimer = _crystalDuration;
+        attackCheckRadius = _attackCheckRadius;
+        canMoving = _canMoving;
+        moveSpeed = _moveSpeed;
+    }
+
+    public void DestroyCrystal()
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+
+        cd.enabled = true;
+        anim.SetBool("Destroy", true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Enemy>() != null)
+        {
+            collision.GetComponent<Enemy>().Damage(transform);
+        }
+    }
+
+    private void FindTarget()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 15);
+
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+            {
+                if (clocestTarget == null ||
+                    (Vector2.Distance(transform.position, clocestTarget.position) > Vector2.Distance(transform.position, hit.transform.position)))
+                    clocestTarget = hit.GetComponent<Enemy>().transform;
+            }
+        }
     }
 }
--- Assets/Scripts/Skills/Crystal_Skill.cs
+++ Assets/Scripts/Skills/Crystal_Skill.cs
@@ -6,9 +6,14 @@ using UnityEngine;
 public class Crystal_Skill : Skill
 {
     [Header("Skill Info")]
+    [SerializeField] private bool canMoving;
+    [SerializeField] private float moveSpeed;
     [SerializeField] private GameObject crystalPrefab;
-    private GameObject currentCrystal;
     [SerializeField] private float crystalDuration;
+    [SerializeField] private float attackCheckRadius;
+    private GameObject currentCrystal;
+
+    Crystal crystalScript;
 
     public override void UseSkill()
     {
@@ -17,12 +22,19 @@ public class Crystal_Skill : Skill
         if (currentCrystal == null)
         {
             currentCrystal = Instantiate(crystalPrefab, player.transform.position, Quaternion.identity);
-            currentCrystal.GetComponent<Crystal>().SetUpCrystal(crystalDuration);
+            crystalScript = currentCrystal.GetComponent<Crystal>();
+            crystalScript.SetUpCrystal(crystalDuration, attackCheckRadius, canMoving, moveSpeed);
         }
         else if (currentCrystal.GetComponent<Crystal>().crystalTimer >= 0)
         {
-            player.transform.position = currentCrystal.transform.position;
-            Destroy(currentCrystal);
+            if (!canMoving)
+            {
+                Vector2 playerPos = player.transform.position;
+                player.transform.position = currentCrystal.transform.position;
+                currentCrystal.transform.position = playerPos;
+
+                crystalScript.DestroyCrystal();
+            }
         }
     }
 }
